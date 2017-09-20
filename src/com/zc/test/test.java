b0fa1cfbd33cdabc8d9d8c57b7e251f131aca79b
QiuZhiCai <1936313417@>
2017-09-20 19:48:48 +0800

    初始化项目，GitHub测试

---
+++ src/com/zc/test/test.java
@@ -0,0 +1,10 @@
+package com.zc.test;
+
+/**
+ * Created by Administrator on 2017/9/20.
+ */
+public class test {
+    public static void main(String[] args){
+        System.out.println("Hello,GitHub!!!");
+    }
+}
